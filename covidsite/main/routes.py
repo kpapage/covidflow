570931c9a8efe4077af90184e5bc0cb5c6c74616
janblake <papchakon@csd.auth.gr>
2021-03-31 03:42:47 +0300

    line chart

--- covidsite/main/routes.py
+++ covidsite/main/routes.py
@@ -8,6 +8,20 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     covidcollection = mongo.db.questions
-    questions = covidcollection.find().count()
+    questions = covidcollection.find()
+    users = len(covidcollection.distinct('owner_id'))
 
-    return render_template('index.html', questions=questions)
+    dates = []
+    dates_and_values = {}
+    for question in questions:
+        dates.append(question['timestamps'][:10])
+
+    sorted_dates = sorted(dates, key=lambda d: tuple(map(int, d.split('-'))))
+
+    for i in range(len(sorted_dates)):
+        dates_and_values[sorted_dates[i]] = sorted_dates.count(sorted_dates[i])
+
+    labels = list(dates_and_values.keys())
+    values = list(dates_and_values.values())
+
+    return render_template('index.html', questions=questions, users=users, labels=labels, values=values)
