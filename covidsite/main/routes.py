9aad0381b0f08ab7edf4569902d3b6789d7f21f2
janblake <papchakon@csd.auth.gr>
2021-04-01 05:35:57 +0300

    wordcloud

--- covidsite/main/routes.py
+++ covidsite/main/routes.py
@@ -13,15 +13,29 @@ def index():
 
     dates = []
     dates_and_values = {}
+    tags = []
+    tags_and_values = {}
+    list_of_tags_and_values = []
     for question in questions:
         dates.append(question['timestamps'][:10])
+        record_tags = question['tags'].split()
+        for i in range(len(record_tags)):
+            tags.append(record_tags[i])
 
     sorted_dates = sorted(dates, key=lambda d: tuple(map(int, d.split('-'))))
 
     for i in range(len(sorted_dates)):
         dates_and_values[sorted_dates[i]] = sorted_dates.count(sorted_dates[i])
 
+    for i in range(len(tags)):
+        tags_and_values[tags[i]] = tags.count(tags[i])
+
+    for key, value in tags_and_values.items():
+        d = {"text": key, "size": value}
+        list_of_tags_and_values.append(d)
+
     labels = list(dates_and_values.keys())
     values = list(dates_and_values.values())
 
-    return render_template('index.html', questions=questions, users=users, labels=labels, values=values)
+    return render_template('index.html', questions=questions, users=users, labels=labels, values=values,
+                           list_of_tags_and_values=list_of_tags_and_values)
