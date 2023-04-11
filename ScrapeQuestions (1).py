a8fd50a94e83a7ba4de7be8e78c47ff05267000e
KostasGeor <koage13.giou@gmail.com>
2023-04-11 09:43:34 +0300

    Automated Scraper to update chromedrivers

--- ScrapeQuestions (1).py
+++ ScrapeQuestions (1).py
@@ -19,7 +19,7 @@ import pymongo
 import pickle
 import ast
 import itertools
-# from webdriver_auto_update import check_driver
+from webdriver_manager.chrome import ChromeDriverManager
 import requests
 import wget
 import zipfile
@@ -271,29 +271,18 @@ def create_classifier():
 
     show_conf_matrix(y_test, y_pred)
 
-url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
-response = requests.get(url)
-version_number = response.text
 
-# build the donwload url
-download_url = "https://chromedriver.storage.googleapis.com/" + version_number + "/chromedriver_win32.zip"
-
-# download the zip file using the url built above
-latest_driver_zip = wget.download(download_url, 'chromedriver.zip')
-
-# extract the zip file
-with zipfile.ZipFile(latest_driver_zip, 'r') as zip_ref:
-    zip_ref.extractall()  # you can specify the destination folder path here
-# delete the zip file downloaded above
-os.remove(latest_driver_zip)
 classifier=pd.read_pickle("xgb_model.pkl")
 options = webdriver.ChromeOptions()
 options.add_argument("user-data-dir=C:\\Users\giou2\\AppData\\Local\\Google\\Chrome\\User Data")
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(
-    executable_path='chromedriver.exe'
-    , options=options)
-
+driver = webdriver.Chrome(ChromeDriverManager().install(),
+    #executable_path='chromedriver.exe',
+    options=options)
+browser_version = driver.capabilities['browserVersion']
+driver_version = driver.capabilities['chrome']['chromedriverVersion'].split(' ')[0]
+print("browser version",browser_version)
+print("driver version",driver_version)
 print('Extracting Question Data')
 
 labels = ['covid*', "coronavirus", "corona-virus", "2019-ncov", "sars-cov"]
